dc41c657f73bd8f2b22d64d431a70384522fa71a
Leedius <tiuy2750@gmail.com>
2023-01-25 12:03:43 +0900

    Initial commit
    # Conflicts:
    #	.idea/workspace.xml
    #	src/com/array/Array01.java
    #	src/com/array/Array02.java
    #	src/com/array/Array03.java
    #	src/com/array/Array04.java
    #	src/com/array/ArrayImportant.java
    #	src/com/array/Ex1_1.java
    #	src/com/array/lk.java
    #	src/com/ex1/Ex1_1.java
    #	src/com/ex1/Ex1_10.java
    #	src/com/ex1/Ex1_11.java
    #	src/com/ex1/Ex1_12.java
    #	src/com/ex1/Ex1_2.java
    #	src/com/ex1/Ex1_3.java
    #	src/com/ex1/Ex1_4.java
    #	src/com/ex1/Ex1_5.java
    #	src/com/ex1/Ex1_6.java
    #	src/com/ex1/Ex1_7.java
    #	src/com/ex1/Ex1_8.java
    #	src/com/ex1/Ex1_9.java
    #	src/com/ex2/Ex2_1.java
    #	src/com/ex2/Ex2_2.java
    #	src/com/ex2/Ex2_5.java
    #	src/com/ex2/Ex2_6.java
    #	src/com/ex2/Ex2_7.java
    #	src/com/ex2/Ex2_8.java

--- src/com/array/Array03.java
+++ src/com/array/Array03.java
@@ -1,10 +1,9 @@
 package com.array;
 
-
 public class Array03 {
     public static void main(String[] args) {
         //배열의 값 읽기
-        int[] arr1={1, 2, 3};
+        int[] arr1 = {1, 2, 3};
 
         //배열의 내용을 출력할때에는 변수명으로 접근 X
         System.out.println(arr1);
@@ -15,14 +14,13 @@ public class Array03 {
         System.out.println(arr1[0]);
         //System.out.println(arr1[3]); 오류. 범위를 벗어났기 때문에
 
-        double[] arr2={1.0, 2.3, 3.6, 4.7, 5.5};
+        double[] arr2 = {1.0, 2.3, 3.6, 4.7, 5.5};
         int i;
-        for(i=0; i<arr2.length; i++){
+        for (i = 0; i < arr2.length; i++) {
             System.out.println(arr2[i]);
         }
         System.out.println();
 
-        System.out.println("배열의 길이 : "+ arr2.length);
-
+        System.out.println("배열의 길이 : " + arr2.length);
     }
 }
